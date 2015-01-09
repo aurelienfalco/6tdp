5dac14fb337979c000d5254badb2cdfee9112404
Nicoeclair <nicoeclair@hotmail.fr>
2015-01-09 14:35:07 +0100

    corrections

--- life_seq.c
+++ life_seq.c
@@ -6,8 +6,6 @@
 //#define PRINT_ALIVE
 #define BS 1000
 
-int num_alive;
-
 #define cell( _i_, _j_ ) board[ ldboard * (_j_) + (_i_) ]
 #define ngb( _i_, _j_ )  nbngb[ ldnbngb * ((_j_) - 1) + ((_i_) - 1 ) ]
 
@@ -23,13 +21,13 @@ void output_board(int N, int *board, int ldboard, int loop)
     int i,j;
     printf("loop %d\n", loop);
     for (i=0; i<N; i++) {
-	for (j=0; j<N; j++) {
-	    if ( cell( i, j ) == 1)
-		printf("X");
-	    else
-		printf(" ");
-	}
-	printf("\n");
+		for (j=0; j<N; j++) {
+			if ( cell( i, j ) == 1)
+				printf("X");
+			else
+				printf(" ");
+		}
+		printf("\n");
     }
 }
 
@@ -37,21 +35,23 @@ void output_board(int N, int *board, int ldboard, int loop)
  * This function generates the iniatl board with one row and one
  * column of living cells in the middle of the board
  */
-void generate_initial_board(int N, int *board, int ldboard)
+int generate_initial_board(int N, int *board, int ldboard)
 {
-    int i, j;
-
-    for (i = 1; i <= BS; i++) {
-	for (j = 1; j <= BS; j++) {
-	    if (i == BS/2 || j == BS/2) {
-		cell(i, j) = 1;
-		num_alive ++;
-	    }
-	    else {
-		cell(i, j) = 0;
-	    }
-	}
+    int i, j, num_alive = 0;
+	
+    for (i = 1; i <= N; i++) {
+		for (j = 1; j <= N; j++) {
+			if (i == N/2 || j == N/2) {
+				cell(i, j) = 1;
+				num_alive ++;
+			}
+			else {
+				cell(i, j) = 0;
+			}
+		}
     }
+
+    return num_alive;
 }
 
 int main(int argc, char* argv[])
@@ -63,11 +63,11 @@ int main(int argc, char* argv[])
  
     int *board;
     int *nbngb;
-
+	
     if (argc < 2) {
-	maxloop = 10;
+		maxloop = 10;
     } else {
-	maxloop = atoi(argv[1]);
+		maxloop = atoi(argv[1]);
     }
     num_alive = 0;
 
@@ -79,7 +79,7 @@ int main(int argc, char* argv[])
     board = malloc( ldboard * ldboard * sizeof(int) );
     nbngb = malloc( ldnbngb * ldnbngb * sizeof(int) );
 
-    generate_initial_board( BS, &(cell(1, 1)), ldboard );
+    num_alive = generate_initial_board( BS, &(cell(1, 1)), ldboard );
 
     //output_board( BS, &(cell(1, 1)), ldboard, 0 );
 
@@ -88,47 +88,47 @@ int main(int argc, char* argv[])
 
     for (loop = 1; loop <= maxloop; loop++) {
 
-	cell(   0, 0   ) = cell(BS, BS);
-	cell(   0, BS+1) = cell(BS,  1);
-	cell(BS+1, 0   ) = cell( 1, BS);
-	cell(BS+1, BS+1) = cell( 1,  1);
+		cell(   0, 0   ) = cell(BS, BS);
+		cell(   0, BS+1) = cell(BS,  1);
+		cell(BS+1, 0   ) = cell( 1, BS);
+		cell(BS+1, BS+1) = cell( 1,  1);
 
-	for (i = 1; i <= BS; i++) {
-	    cell(   i,    0) = cell( i, BS);
-	    cell(   i, BS+1) = cell( i,  1);
-	    cell(   0,    i) = cell(BS,  i);
-	    cell(BS+1,    i) = cell( 1,  i);
-	}
-
-	for (i = 1; i <= BS; i++) {
-	    for (j = 1; j <= BS; j++) {
-		ngb( i, j ) =
-		    cell( i-1, j-1 ) + cell( i, j-1 ) + cell( i+1, j-1 ) +
-		    cell( i-1, j   ) +                  cell( i+1, j   ) +
-		    cell( i-1, j+1 ) + cell( i, j+1 ) + cell( i+1, j+1 );
-	    }
-	}
-
-	num_alive = 0;
-	for (i = 1; i <= BS; i++) {
-	    for (j = 1; j <= BS; j++) {
-		if ( (ngb( i, j ) < 2) || 
-		     (ngb( i, j ) > 3) ) {
-		    cell(i, j) = 0;
+		for (i = 1; i <= BS; i++) {
+			cell(   i,    0) = cell( i, BS);
+			cell(   i, BS+1) = cell( i,  1);
+			cell(   0,    i) = cell(BS,  i);
+			cell(BS+1,    i) = cell( 1,  i);
 		}
-		else {
-		    if ((ngb( i, j )) == 3)
-			cell(i, j) = 1;
-		}
-		if (cell(i, j) == 1) {
-		    num_alive ++;
-		}
-	    }
-	}
 
-	//output_board( BS, &(cell(1, 1)), ldboard, loop);
+		for (i = 1; i <= BS; i++) {
+			for (j = 1; j <= BS; j++) {
+				ngb( i, j ) =
+					cell( i-1, j-1 ) + cell( i, j-1 ) + cell( i+1, j-1 ) +
+					cell( i-1, j   ) +                  cell( i+1, j   ) +
+					cell( i-1, j+1 ) + cell( i, j+1 ) + cell( i+1, j+1 );
+			}
+		}
+
+		num_alive = 0;
+		for (i = 1; i <= BS; i++) {
+			for (j = 1; j <= BS; j++) {
+				if ( (ngb( i, j ) < 2) || 
+					 (ngb( i, j ) > 3) ) {
+					cell(i, j) = 0;
+				}
+				else {
+					if ((ngb( i, j )) == 3)
+						cell(i, j) = 1;
+				}
+				if (cell(i, j) == 1) {
+					num_alive ++;
+				}
+			}
+		}
+
+		//output_board( BS, &(cell(1, 1)), ldboard, loop);
 #ifdef PRINT_ALIVE
-	printf("%d \n", num_alive);
+		printf("%d \n", num_alive);
 #endif
     }
 
