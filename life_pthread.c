bd1d53924d4f114023420b5e5aea72b2045d1374
Nicoeclair <nicoeclair@hotmail.fr>
2015-01-29 14:34:47 +0100

    avancement pthread

--- life_pthread.c
+++ life_pthread.c
@@ -1,50 +1,65 @@
-#include "util.h"
 #include <pthread.h>
 #include <semaphore.h>
 
-struct Param {
-	int thread_id;
-};
+#include "util.h"
+#include "common.h"
 
-sem_t *sems;
+sem_t *sems_neig;
+//sem_t *sems_maj;
 pthread_t *threads;
-int *board;
-int *nbngb;
 int nb_threads;
-int ldboard, ldnbngb;
+int threads_per_side;
 int block_size;
 
 void* start_work(void *param) {
-	int loop, i, j, num_alive, thread_id = (int)param;
-	int istart = (thread_id / nb_threads) * block_size + 1;
-	int iend = ((thread_id / nb_threads) + 1) * block_size;
-	int jstart = (thread_id % nb_threads) * block_size + 1;
-	int jend = ((thread_id % nb_threads) + 1) * block_size;
+    int loop, i, j, num_alive, thread_id = (int)param, count;
+	int istart = (thread_id / threads_per_side) * block_size + 1;
+	int iend = ((thread_id / threads_per_side) + 1) * block_size;
+	int jstart = (thread_id % threads_per_side) * block_size + 1;
+	int jend = ((thread_id % threads_per_side) + 1) * block_size;
+	int end_num = threads_per_side * block_size;
 	
-	if (thread_id == 0) {
+	if (thread_id == 1) {
+		printf("block_size=%d\n", block_size);
 		printf("proc=%d\n", thread_id);
 		printf("maxloop=%d\n", maxloop);
 		printf("istart=%d\n", istart);
 		printf("iend=%d\n", iend);
 		printf("jstart=%d\n", jstart);
 		printf("jend=%d\n", jend);
+		printf("end_num=%d\n", end_num);
 	}
 	
- 	for (loop = 1; loop <= maxloop; loop++) {
-		// copie des cellules dans les coins
- 		cell(   0, 0   ) = cell(nb_threads*block_size, nb_threads*block_size);
- 		cell(   0, nb_threads*block_size+1) = cell(nb_threads*block_size,  1);
- 		cell(nb_threads*block_size+1, 0   ) = cell( 1, nb_threads*block_size);
- 		cell(nb_threads*block_size+1, nb_threads*block_size+1) = cell( 1,  1);
+    for (loop = 1; loop <= maxloop; loop++) {
+        // Chaque proc copie ses données dans les cellules fantomes associées à ses cellules
+		if (istart == 1) {
+			if (jstart == 1)
+				cell(end_num+1, end_num+1) = cell(1, 1);
+			else if (jend == end_num)
+				cell(end_num+1, 0) = cell(1, end_num);
+			for (j = jstart; j <= jend; j++) {
+				cell(end_num+1, j) = cell(1, j);
+			}
+		}
+		else if (iend == end_num) {
+			if (jstart == 1)
+				cell(0, end_num+1) = cell(end_num, 1);
+			else if (jend == end_num)
+				cell(0, 0) = cell(end_num, end_num);
+			for (j = jstart; j <= jend; j++) {
+				cell(0, j) = cell(end_num, j);
+			}
+		}
+		if (jstart == 1) {
+			for (i = istart; i <= iend; i++)
+				cell(i, end_num+1) = cell(i, 1);
+		}
+		else if (jend == end_num) {
+			for (i = istart; i <= iend; i++)
+				cell(i, 0) = cell(i, end_num);
+		}
 		
-		// copie des cellules des côtés
- 		for (i = istart; i <= iend; i++) {
- 			cell(   i,    istart) = cell( i, BS);
- 			cell(   i,    iend+1) = cell( i,  1);
- 			cell(   istart,    i) = cell(BS,  i);
- 			cell(iend+1,       i) = cell( 1,  i);
- 		}
-
+		// Calcul des voisins
  		for (j = jstart; j <= jend; j++) {
  			for (i = istart; i <= iend; i++) {
  				ngb( i, j ) =
@@ -53,7 +68,20 @@ void* start_work(void *param) {
 					cell( i-1, j+1 ) + cell( i, j+1 ) + cell( i+1, j+1 );
  			}
  		}
-
+		
+		for (i = thread_id-1; i <= thread_id+1; i++) {
+			for (i = thread_id-1; i <= thread_id+1; i++) {
+				sem_post(&sems_neig[thread_id]);
+			}
+		}		
+		
+		count = 8;
+		while (count) {
+			sem_wait(&sems_neig[thread_id]);
+			count--;
+		}
+		
+		// MAJ
  		num_alive = 0;
  		for (j = jstart; j <= jend; j++) {
  			for (i = istart; i <= iend; i++) {
@@ -71,40 +99,35 @@ void* start_work(void *param) {
 			}
 		}
 	}
-	
+
 	return NULL;
 }
 
 int main(int argc, char* argv[])
 {
- 	int i, num_alive;
+ 	int i;
  	double t1, t2;
  	double temps;
-	
-	nb_threads = 4;
+    
+    threads_per_side = 2;
+	nb_threads = threads_per_side * threads_per_side;
  	
  	get_arg(argc,argv,NULL,NULL);
- 	num_alive = 0;
-
-    /* Leading dimension of the board array */
- 	ldboard = BS + 2;
-    /* Leading dimension of the neigbour counters array */
- 	ldnbngb = BS;
+	init();
 	
- 	board = malloc( ldboard * ldboard * sizeof(int) );
- 	nbngb = malloc( ldnbngb * ldnbngb * sizeof(int) );
-	
- 	num_alive = generate_initial_board( BS, &(cell(1, 1)), ldboard );
-	
-    //output_board( BS, &(cell(1, 1)), ldboard, 0 );
-	
+	if (print) 
+    	output_board( BS, &(cell(1, 1)), ldboard, maxloop);
+    
  	printf("Starting number of living cells = %d\n", num_alive);
  	t1 = mytimer();
 	
-	threads = malloc(nb_threads * sizeof(pthread_t));
-	sems = malloc(nb_threads * sizeof(sem_t));
-	for (i = 0; i < nb_threads; i++) {
-		if (sem_init(&sems[i], 0, 0)) {
+    threads = malloc(nb_threads * sizeof(pthread_t));
+    sems_neig = malloc(nb_threads * sizeof(sem_t));
+
+    block_size = BS / threads_per_side;
+
+    for (i = 0; i < nb_threads; i++) {
+        if (sem_init(&sems_neig[i], 0, 0)) {
 			fprintf(stderr, "cannot create semaphore\n");
 			return -1;
 		}
@@ -113,7 +136,7 @@ int main(int argc, char* argv[])
 	
 	for (i = 0; i < nb_threads; i++) {
 		pthread_join(threads[i], NULL);
-		sem_destroy(&sems[i]);
+		sem_destroy(&sems_neig[i]);
 	}
 
 	t2 = mytimer();
@@ -121,7 +144,19 @@ int main(int argc, char* argv[])
 	printf("Final number of living cells = %d\n", num_alive);
 	printf("time=%.2lf ms\n",(double)temps * 1.e3);
 
-    //output_board( BS, &(cell(1, 1)), ldboard, maxloop);
+	int j;
+	for (i=1; i<=BS; i++) {
+		for (j=1; j<=BS; j++) {
+				printf("%d", ngb(i, j));
+		}
+		printf("\n");
+	}
+
+	if (print) {
+    	output_board( BS, &(cell(1, 1)), ldboard, maxloop);
+		printf("----------------------------\n");
+    	output_board( BS+2, &(cell(0, 0)), ldboard, maxloop);
+	}
 
 	free(board);
 	free(nbngb);
